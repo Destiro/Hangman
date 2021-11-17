787cdb3616726d05801534c6e2de6ad437ccd0fe
Connor de Bruyn <connor.debruyn@xero.com>
2021-11-17 16:25:28 +1300

    Skeleton code for project

---
+++ Hangman/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Hangman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine("Please Enter a character");
+        }
+        
+        
+    }
+}
