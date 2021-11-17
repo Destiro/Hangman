f4c4d1a09e1edef676af4d1b22bf6ec12e7ca453
Connor de Bruyn <connor.debruyn@xero.com>
2021-11-17 16:44:34 +1300

    Added empty methods and data files

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -4,12 +4,30 @@ namespace Hangman
 {
     class Program
     {
+        private int lives = 3;
+        private String word = "";
+        private int gamesWon = 0;
+        private Boolean hasLost = false;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.ReadLine("Please Enter a character");
+            //Console.ReadLine("Please Enter a character");
         }
-        
-        
+
+        public String GenerateWord()
+        {
+            return "hello"; //todo read from data
+        }
+
+        public void TakeTurn()
+        {
+            //todo a turn for each guess
+        }
+
+        public void DrawHangman()
+        {
+            //todo console log a string of the hangman guy
+        }
     }
 }
