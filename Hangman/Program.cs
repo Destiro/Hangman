ecd168072076e3fc0b7bf2ac4946278e0b7b915c
connor.debruyn <connordebruyn234@gmail.com>
2021-11-18 13:41:58 +1300

    Draft for Version 2 - Draws Hangman

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -11,6 +11,7 @@ namespace Hangman
         private static StringBuilder guessedWord = new StringBuilder("");
         private static Boolean hasLost = false;
         private static ArrayList guesses = new ArrayList();
+
         private static int turn = 0;
         //private static int gamesWon = 0;
 
@@ -35,20 +36,26 @@ namespace Hangman
 
         private static void GenerateWord()
         {
-            word = "rhythm"; //todo read from data
-            guessedWord = new StringBuilder("______");
+            string[] lines = System.IO.File.ReadAllLines("../../.././data/words.txt");
+            word = lines[new Random().Next(lines.Length)];
+            guessedWord = new StringBuilder();
+
+            for(int i=0; i<word.Length; i++)
+                guessedWord.Append('_');
         }
-        
+
         private static void DrawHangman()
         {
-            //todo console log a string of the hangman guy from file using mods
+            Console.Clear();
+            string[] lines = System.IO.File.ReadAllLines($"../../.././data/hangman_{lives}.txt");
+            foreach (var line in lines)
+                Console.WriteLine(line);
         }
 
         private static void TakeTurn()
         {
             Console.WriteLine("----------------------------------");
-            
-            Console.WriteLine($"Turn: {turn} || Lives: "+lives+" || Current word : "+guessedWord);
+            Console.WriteLine($"Turn: {turn} || Lives: {lives} || Current word : {guessedWord}");
             Console.Write("Current Guesses: ");
 
             foreach (char letter in guesses)
@@ -58,17 +65,17 @@ namespace Hangman
 
             Console.Write("\nPlease take a guess: ");
             var guess = Console.ReadLine();
+            guess = guess?.ToLower();
+
             if (guess is {Length: 1})
             {
-                CheckGuess(char.Parse(guess)); 
+                CheckGuess(char.Parse(guess));
             }
             else
             {
                 Console.WriteLine("----------------------------------");
                 Console.WriteLine("Invalid Guess! Please enter 1 character.");
             }
-            
-            
         }
 
         private static void CheckGuess(char guess)
@@ -78,22 +85,22 @@ namespace Hangman
                 Console.WriteLine("----------------------------------");
                 Console.WriteLine("You have already guessed this letter!");
                 return;
-            } 
+            }
+
+            guesses.Add(guess);
+            turn++;
+            
+            if (word.Contains(guess))
+            {
+                for (var i = 0; i < word.Length; i++)
+                {
+                    if (word[i].Equals(guess))
+                        guessedWord[i] = guess;
+                }
+            }
             else
             {
-                guesses.Add(guess);
-                if (word.Contains(guess))
-                {
-                    for (var i = 0; i < word.Length; i++)
-                    {
-                        if (word[i].Equals(guess))
-                            guessedWord[i] = guess;
-                    }
-                }
-                else
-                {
-                    lives--;
-                }
+                lives--;
             }
         }
 
@@ -101,12 +108,13 @@ namespace Hangman
         {
             if (lives == 0)
             {
+                DrawHangman();
                 Console.WriteLine("----------------------------------");
                 Console.WriteLine("You have lost the Game!");
-                Console.WriteLine("Word = "+word+", Current word = "+guessedWord);
+                Console.WriteLine($"Word = {word}, Current word = {guessedWord}");
                 hasLost = true;
             }
-            else if(guessedWord.Equals(word))
+            else if (guessedWord.Equals(word))
             {
                 Console.WriteLine("----------------------------------");
                 Console.WriteLine("You have won the game!");
@@ -114,7 +122,6 @@ namespace Hangman
                 //todo reset all variables
                 hasLost = true; //todo change this
             }
-            
         }
     }
 }
