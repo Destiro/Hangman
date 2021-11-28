2a33d0d3815725312e053dfda443b022016770c7
connor.debruyn <connordebruyn234@gmail.com>
2021-11-29 09:33:45 +1300

    Refactored game class to library and removed unused method

--- Hangman/Word.cs
+++ Hangman/Word.cs
@@ -16,15 +16,6 @@ namespace Hangman
             return _word.Contains(guess);
         }
 
-        public StringBuilder AddGuesses(StringBuilder currGuesses, char guess)
-        {
-            for (var i = 0; i < _word.Length; i++)
-                if (_word[i].Equals(guess))
-                    currGuesses[i] = guess;
-            
-            return currGuesses;
-        }
-
         public string GetWord()
         {
             return _word;
